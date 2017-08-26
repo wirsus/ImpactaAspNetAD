7e5705266153b3c61ff7c897483041b5edabb281
sa3511 <sa3511@4P35_11>
2017-08-26 13:23:57 -0300

    Add project files.

---
+++ Northwind.Repositorios.SqlServer.Ado/CategoriaRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Northwind.Repositorios.SqlServer.Ado
+{
+    public class CategoriaRepositorio
+    {
+        public DataTable Selecionar()
+        {
+            var categoriaDataTable = new DataTable();
+
+            using (var conexao = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True"))
+            {
+                conexao.Open();
+
+                var instrucao = @"SELECT CategoryID, CategoryName FROM Categories;";
+
+                using (var comando = new SqlCommand(instrucao, conexao))
+                {
+                    using (var dataAdapter = new SqlDataAdapter(comando))
+                    {
+                        dataAdapter.Fill(categoriaDataTable);
+                    }
+                }
+
+                //conexao.Close();
+            }
+            return categoriaDataTable;
+        }
+    }
+}
---
+++ Northwind.Repositorios.SqlServer.AdoTests/CategoriaRepositorioTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
+
+
+namespace Northwind.Repositorios.SqlServer.Ado.Tests
+{
+    [TestClass()]
+    public class CategoriaRepositorioTests
+    {
+        [TestMethod()]
+        public void SelecionarTest()
+        {
+            var categoriaRepositorio = new CategoriaRepositorio();
+
+            var categoriasDataTable = categoriaRepositorio.Selecionar();
+
+            foreach (DataRow registro in categoriasDataTable.Rows)
+            {
+                Console.WriteLine($"{registro[0]} - {registro[1]}");
+            }
+            
+
+        }
+    }
+}
