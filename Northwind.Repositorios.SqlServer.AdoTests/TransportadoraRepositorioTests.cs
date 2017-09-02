ef5a100c81c3ae7805ef8d642a91632ad5318588
sa3511 <sa3511@4P35_11>
2017-09-02 14:10:26 -0300

    Adicionado Selecionar Test a App Config com a string de conexão

---
+++ Northwind.Repositorios.SqlServer.AdoTests/TransportadoraRepositorioTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Northwind.Repositorios.SqlServer.Ado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Northwind.Repositorios.SqlServer.Ado.Tests
+{
+    [TestClass()]
+    public class TransportadoraRepositorioTests
+    {
+        TransportadoraRepositorio _repositorio = new TransportadoraRepositorio();
+
+        [TestMethod()]
+        public void SelecionarTest()
+        {
+            Assert.IsTrue(_repositorio.Selecionar().Any());
+        }
+
+    }
+}
