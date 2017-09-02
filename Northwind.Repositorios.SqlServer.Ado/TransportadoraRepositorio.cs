6d7406afabd9174ddd1d38f243a72f7027f8aac2
sa3511 <sa3511@4P35_11>
2017-09-02 15:37:48 -0300

    Metodo de Inserção criado e testado

--- Northwind.Repositorios.SqlServer.Ado/TransportadoraRepositorio.cs
+++ Northwind.Repositorios.SqlServer.Ado/TransportadoraRepositorio.cs
@@ -39,6 +39,66 @@ namespace Northwind.Repositorios.SqlServer.Ado
             return transportadoras;
         }
 
+        public Transportadora Selecionar(int ID)
+        {
+
+            Transportadora transportadora = null;
+
+            using (var conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["northwindConnectionString"].ConnectionString))
+            {
+                conexao.Open();
+                const string nomeProcedure = "TransportadoraSelecionar";
+
+                using (var comando = new SqlCommand(nomeProcedure, conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddWithValue("ShipperID", ID);
+                    //comando.Parameters.Add(new SqlParameter("ShipperID", ID));
+
+                    using (var registro = comando.ExecuteReader())
+                    {
+                        if (registro.Read())
+                        {
+                            transportadora = Mapear(registro);
+                        }
+                    }
+                }
+            }
+
+            return transportadora;
+        }
+
+        public void Inserir(Transportadora transportadora)
+        {
+            using (var conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["northwindConnectionString"].ConnectionString))
+            {
+                conexao.Open();
+                const string nomeProcedure = "TransportadoraInserir";
+
+                using (var comando = new SqlCommand(nomeProcedure, conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddRange(Mapear(transportadora));
+
+                    //comando.ExecuteNonQuery();
+                    transportadora.ID = Convert.ToInt32(comando.ExecuteScalar());
+
+                }
+            }
+
+        }
+
+        private SqlParameter[] Mapear(Transportadora transportadora)
+        {
+            var parametros = new List<SqlParameter>();
+
+            parametros.Add(new SqlParameter("CompanyName", transportadora.Nome));
+            parametros.Add(new SqlParameter("Phone", transportadora.Telefone ));
+
+
+            return parametros.ToArray();
+        }
+
         private Transportadora Mapear(SqlDataReader registro)
         {
             var transportadora = new Transportadora();
--- Northwind.Repositorios.SqlServer.AdoTests/TransportadoraRepositorioTests.cs
+++ Northwind.Repositorios.SqlServer.AdoTests/TransportadoraRepositorioTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Northwind.Dominio;
 using Northwind.Repositorios.SqlServer.Ado;
 using System;
 using System.Collections.Generic;
@@ -20,5 +21,23 @@ namespace Northwind.Repositorios.SqlServer.Ado.Tests
             Assert.IsTrue(_repositorio.Selecionar().Any());
         }
 
+        [TestMethod()]
+        public void SelecionarComIdTest()
+        {
+            Assert.IsNotNull(_repositorio.Selecionar(1));
+            Assert.IsNull(_repositorio.Selecionar(4561));
+        }
+
+        [TestMethod()]
+        public void InserirTest()
+        {
+            var transportadora = new Transportadora();
+            transportadora.Nome = "Teste Transportes";
+            transportadora.Telefone = "(111) 111-1111";
+
+            _repositorio.Inserir(transportadora);
+
+            Assert.AreNotEqual(0, transportadora.ID);
+        }
     }
 }
