7bd82e79f39c7c31100cbd0042cfaa522a4abcb9
sa3511 <sa3511@4P35_11>
2017-09-23 14:11:16 -0300

    Criados as view e controlers para  a loja usando DBcontext

---
+++ Loja.Mvc/Helpers/Mapeamento.cs
@@ -0,0 +1,59 @@
+﻿using Loja.Dominio;
+using Loja.Mvc.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System;
+
+namespace Loja.Mvc.Helpers
+{
+    public static class Mapeamento
+    {
+        public static List<ProdutoViewModel> Mapear(List<Produto> produtos)
+        {
+            var produtosViewModel = new List<ProdutoViewModel>();
+
+            foreach (var produto in produtos)
+            {
+                produtosViewModel.Add(Mapear(produto));
+            }
+
+            return produtosViewModel;
+        } 
+
+        public static Produto Mapear(ProdutoViewModel viewModel, Categoria categoria)
+        {
+            var produto = new Produto();
+
+            produto.Id = viewModel.Id;
+            produto.Categoria = categoria;
+            produto.Estoque = viewModel.Estoque.Value;
+            produto.Nome = viewModel.Nome;
+            produto.Preco = viewModel.Preco.Value;
+
+            return produto;
+        }
+
+        public static ProdutoViewModel Mapear(Produto produto, List<Categoria> categorias = null)
+        {
+            var viewModel = new ProdutoViewModel();
+
+            viewModel.CategoriaId = produto.Categoria?.ID;
+            viewModel.CategoriaNome = produto.Categoria?.Nome;
+
+            if (categorias != null)
+            {
+                foreach (var categoria in categorias)
+                {
+                    viewModel.Categorias.Add(new SelectListItem { Text = categoria.Nome, Value = categoria.ID.ToString() });
+                }
+            }
+
+            viewModel.Estoque = produto.Estoque;
+            viewModel.Id = produto.Id;
+            viewModel.Nome = produto.Nome;
+            viewModel.Preco = produto.Preco;
+
+            return viewModel;
+        }
+    }
+}
---
+++ Loja.Mvc/Models/ProdutoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Loja.Mvc.Models
+{
+    public class ProdutoViewModel
+    {
+        public ProdutoViewModel()
+        {
+            Categorias = new List<SelectListItem>();
+        }
+        public Int32 Id { get; set; }
+        
+        [Required(ErrorMessage = "Digite o nome!")]
+        public String Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o preço!")]
+        [Display(Name = "Preço")]
+        public Decimal? Preco { get; set; }
+
+        [Required(ErrorMessage = "Digite o valor do estoque!")]
+        public Nullable<Int32> Estoque { get; set; }
+
+        [Display(Name = "Categoria")]
+        public String CategoriaNome { get; set; }
+
+        [Required]
+        [Display(Name = "Categoria")]
+        public Nullable<Int32> CategoriaId { get; set; }
+
+        public List<SelectListItem> Categorias { get; set; }
+    }
+}
